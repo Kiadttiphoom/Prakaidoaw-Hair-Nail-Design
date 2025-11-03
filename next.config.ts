268e441c1371719a567b7111dd3abc1b78b81d3f
Kiadttiphoom Booiad <kiadttiphoom29@gmail.com>
2025-11-03 16:59:09 +0700

    update to TailwindCSS v4 & fix build config

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
-
-  compiler: {
-    reactRemoveProperties: true,
+  reactStrictMode:false,
+  compiler : {
+    reactRemoveProperties:true,
   },
-  modularizeImports: {
-    "lucide-react": {
-      transform: "lucide-react/{{member}}",
-    },
-  },
-
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "**",
-      },
-    ],
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [360, 480, 640, 768, 1024, 1280, 1536],
-    imageSizes: [200, 300, 400, 500, 600, 800],
+    domains: ["images.unsplash.com"],
+    formats: ["image/webp"],
+    deviceSizes: [360, 480, 640, 768, 1024, 1280],
+    imageSizes: [200, 300, 400, 500, 600],
   },
 };
 
